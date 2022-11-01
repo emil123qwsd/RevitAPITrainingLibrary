9a5ef9108f2520d2e6c6145e59d06a882376b31c
эмиль ахметханов <emil.ah@yandex.ru>
2022-11-01 21:05:47 +0300

    Добавьте файлы проекта.

---
+++ SelectionUtils.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingLibrary
+{
+    public class SelectionUtils
+    {
+        public static Element PickObject(ExternalCommandData commandData, string massage = "Выберите элемент")
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var selectedObject = uidoc.Selection.PickObject(ObjectType.Element, massage);
+            var oElement = doc.GetElement(selectedObject);
+            return oElement;
+        }
+
+        public static List<Element> PickObjects(ExternalCommandData commandData, string massage = "Выберите элементы")
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var selectedObjects = uidoc.Selection.PickObjects(ObjectType.Element, massage);
+            List<Element> elementList = selectedObjects.Select(selectedObject => doc.GetElement(selectedObject)).ToList();
+            return elementList;
+        }
+    }
+}
---
+++ WallsUtils.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingLibrary
+{
+    public class WallsUtils
+    {
+        public static List<Wall> GetWallSystems(ExternalCommandData commandData) 
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<Wall> WallSystemTypes = new FilteredElementCollector(doc)
+                                                     .OfClass(typeof(Wall))
+                                                     .Cast<Wall>()
+                                                     .ToList();
+            return WallSystemTypes;
+        }
+    }
+}
