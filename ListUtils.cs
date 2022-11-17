3137eadda25a9caa82689c34382ec403112145ca
эмиль ахметханов <emil.ah@yandex.ru>
2022-11-17 23:29:00 +0300

    7

---
+++ ListUtils.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingLibrary
+{
+    public class ListUtils
+    {
+        public static List<ViewSheet> GetSheet(Document doc) 
+        {
+            var TitleBlocks = new FilteredElementCollector(doc)
+               .OfCategory(BuiltInCategory.OST_TitleBlocks)
+               .WhereElementIsElementType()
+               .OfType<FamilySymbol>()
+               .ToList();
+        }
+           
+    }
+}
